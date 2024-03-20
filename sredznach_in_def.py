fc715248c6ee372a827e79c40fcf6f6f0d08e17a
Fedorissimo228 <148334003+Fedorissimo228@users.noreply.github.com>
2024-03-20 12:19:18 +0300

    Create sredznach_in_def.py

---
+++ sredznach_in_def.py
@@ -0,0 +1,13 @@
+b=0
+c=0
+print("ВВЕДИТЕ ПОСЛЕДОВАТЕЛЬНОСТЬ ЧИСЕЛ НА РАЗНЫХ СТРОЧКАХ, КОТОРАЯ ЗАКАНЧИВАЕТСЯ 0")
+def sredznach():
+  a = float(input())
+  c+=1
+  while a!=0:
+    b+=a
+    a = float(input())
+    c+=1
+  b=b:c
+  return b
+sredznach()
